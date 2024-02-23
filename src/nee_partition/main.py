c8676554a093175cd3f172738443e0fff3e2359b
Erkka Rinne <erkka.rinne@fmi.fi>
2024-02-23 15:45:09 +0200

    Make data file path a parameter of the main function

--- src/nee_partition/main.py
+++ src/nee_partition/main.py
@@ -1,22 +1,25 @@
 """Main module"""
 
+from pathlib import Path
+
 import pandas as pd
-from pyprojroot import here
 
 from nee_partition import respiration
 
 VARIABLES = {"temperature": "TA", "ppfd": "PAR", "nee": "NEE"}
+INDEX_COL = "TIMESTAMP_END"
 PPFD_THRESHOLD = 20  # µmol m-2 s-1
 
-alldata = pd.read_csv(
-    here() / "Kalevansuo.csv", index_col="TIMESTAMP_END", parse_dates=True
-).rename(columns={data_var: variable for variable, data_var in VARIABLES.items()})
 
-
-def main():
+def main(data_file_path: Path | str):
     """Partition NEE into GPP and TER"""
 
-    night_data = alldata.where(alldata["ppfd"] < PPFD_THRESHOLD)[
+    # Read in data
+    data = pd.read_csv(data_file_path, index_col=INDEX_COL, parse_dates=True).rename(
+        columns={data_var: variable for variable, data_var in VARIABLES.items()}
+    )
+
+    night_data = data.where(data["ppfd"] < PPFD_THRESHOLD)[
         ["temperature", "nee"]
     ].dropna()
     temp_sensitivity, temp_sensitivity_err = respiration.find_temperature_sensitivity(
